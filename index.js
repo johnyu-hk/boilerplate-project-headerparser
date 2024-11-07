cfee45dd5a13fdd8c3b543defde6001cca005653
john coding <johnsonyu97@gmail.com>
2024-11-07 07:44:33 +0000

    Request Header Parser Microservice

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false })).use(bodyParser.json());
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -22,6 +25,14 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', function (req, res) {
+  res.json({ data: req.headers,
+    "ipaddress": req.headers["x-forwarded-for"],
+    "language": req.headers["accept-language"],
+    "software": req.headers["user-agent"],
+   });
 });
 
 // listen for requests :)
